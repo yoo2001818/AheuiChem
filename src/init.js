9d1bbb987514c7adf54dfd0100c6e5f367ee6273
Duknam Yoo <yoo2001818@gmail.com>
2015-06-18 12:22:59 +0900

    Implement client code to run Aheui interpreter

--- src/init.js
+++ src/init.js
@@ -1,5 +1,20 @@
 // Entry point of the application
 
+var parser = require('./parser');
+var Renderer = require('./renderer');
+var Interpreter = require('./interpreter');
+
+var interpreter;
+var renderer;
+
 window.onload = function() {
-  document.getElementById('viewport').innerHTML = 'Hello, world!';
+  var code = document.getElementById('source').innerHTML;
+  interpreter = new Interpreter(code);
+  renderer = new Renderer(document.getElementById('viewport'), interpreter);
+  setInterval(function() {
+    renderer.preNext();
+    interpreter.next();
+    renderer.postNext();
+  }, 10);
+  window.interpreter = interpreter;
 }
---
+++ src/renderer.js
@@ -0,0 +1,53 @@
+var TileMap = require('./tilemap');
+var parser = require('./parser');
+
+var Renderer = function(viewport, interpreter) {
+  this.viewport = viewport;
+  this.interpreter = interpreter;
+  this.reset();
+}
+
+Renderer.prototype.reset = function() {
+  this.domMap = new TileMap(this.interpreter.map.width, this.interpreter.map.height);
+  this.createNodes();
+}
+
+Renderer.prototype.createNodes = function() {
+  // Clears nodes in viewport
+  while(this.viewport.firstChild) {
+    this.viewport.removeChild(this.viewport.firstChild);
+  }
+  // Adds nodes in viewport
+  for(var y = 0; y < this.domMap.height; ++y) {
+    var row = document.createElement('tr');
+    this.viewport.appendChild(row);
+    for(var x = 0; x < this.domMap.width; ++x) {
+      var column = document.createElement('td');
+      row.appendChild(column);
+      column.tx = x;
+      column.ty = y;
+      this.domMap.set(x, y, column);
+      this.updateNode(x, y);
+    }
+  }
+}
+
+Renderer.prototype.updateNode = function(x, y) {
+  var node = this.domMap.get(x, y);
+  var tile = this.interpreter.map.get(x, y);
+  if(tile) node.innerHTML = parser.encodeSyllable(tile);
+}
+
+Renderer.prototype.preNext = function() {
+  var state = this.interpreter.state;
+  var node = this.domMap.get(state.x, state.y);
+  node.className = "called";
+}
+
+Renderer.prototype.postNext = function() {
+  var state = this.interpreter.state;
+  var node = this.domMap.get(state.x, state.y);
+  node.className = "running";
+}
+
+module.exports = Renderer;
