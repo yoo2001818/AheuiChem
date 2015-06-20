de2767f8aa7d0aa772b62de4cb20b1341a015ba3
Duknam Yoo <yoo2001818@gmail.com>
2015-06-20 20:08:04 +0900

    Change to predict 'on demand'

--- src/init.js
+++ src/init.js
@@ -7,21 +7,39 @@ var Predictor = require('./predictor');
 
 var interpreter;
 var renderer;
+var predictor;
 
 window.onload = function() {
   var code = document.getElementById('source').innerHTML;
   interpreter = new Interpreter(code);
   var predictQuota = interpreter.map.width * interpreter.map.height * 2;
-  var predictor = new Predictor(interpreter.map);
+  predictor = new Predictor(interpreter.map);
   for(var i = 0; i < predictQuota; ++i) {
     if(!predictor.next()) break;
-    predictor.updated = [];
   }
+  predictor.updated = [];
   renderer = new Renderer(document.getElementById('viewport'), interpreter);
   setInterval(function() {
     renderer.preNext();
     interpreter.next();
+    // Predict
+    
+    predictor.stack = [{
+      segment: predictor.segments.length,
+      x: interpreter.state.x,
+      y: interpreter.state.y,
+      direction: {
+        x: interpreter.state.direction.x,
+        y: interpreter.state.direction.y
+      }
+    }];
+    predictor.segments.push([]);
+    for(var i = 0; i < predictQuota; ++i) {
+      if(!predictor.next()) break;
+    }
+    interpreter.updated = interpreter.updated.concat(predictor.updated);
+    predictor.updated = [];
     renderer.postNext();
-  }, 20);
+  }, 1000);
   window.interpreter = interpreter;
 }
