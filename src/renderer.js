e9f7dc2c2a2cc33cf7627f606e9df1f895dfa0d4
Duknam Yoo <yoo2001818@gmail.com>
2015-06-21 15:56:40 +0900

    Implement drawing command icons in Renderer

--- src/renderer.js
+++ src/renderer.js
@@ -2,6 +2,13 @@ var TileMap = require('./tilemap');
 var CanvasLayer = require('./canvaslayer');
 var parser = require('./parser');
 
+var Hangul = {
+  initial: 'ㄱㄲㄴㄷㄸㄹㅁㅂㅃㅅㅆㅇㅈㅉㅊㅋㅌㅍㅎ'.split(''),
+  medial: 'ㅏㅐㅑㅒㅓㅔㅕㅖㅗㅘㅙㅚㅛㅜㅝㅞㅟㅠㅡㅢㅣ'.split(''),
+  final: ' ㄱㄲㄳㄴㄵㄶㄷㄹㄺㄻㄼㄽㄾㄿㅀㅁㅂㅄㅅㅆㅇㅈㅊㅋㅌㅍㅎ'.split(''),
+  code: 0xAC00
+};
+
 // TODO is Array good method to do this?
 var arrowMap = {
   up: [2, 0],
@@ -18,7 +25,6 @@ var arrowMap = {
   reverse: [0, 1]
 };
 
-// TODO it's really messy, should change it soon.
 var pathMap = {
   'up': [2, 1],
   'left': [1, 2],
@@ -40,6 +46,30 @@ var segmentMap = [
   [0, 1],
   [1, 1]
 ];
+
+var commandMap = {
+  'none': [0, 0],
+  'end': [1, 0],
+  'add': [2, 0],
+  'multiply': [3, 0],
+  'subtract': [2, 1],
+  'divide': [3, 1],
+  'mod': [1, 1],
+  'push': [0, 1],
+  // TODO should add icons for pop, push to/from stdio
+  'push-unicode': [0, 1],
+  'push-number': [0, 1],
+  'pop': [0, 2],
+  // TODO should add icons for pop, push to/from stdio
+  'pop-unicode': [0, 2],
+  'pop-number': [0, 2],
+  'copy': [1, 2],
+  'flip': [2, 2],
+  'select': [3, 2],
+  'move': [0, 3],
+  'compare': [1, 3],
+  'condition': [2, 3]
+};
 
 // http://stackoverflow.com/a/3368118/3317669
 function roundRect(ctx, x, y, width, height, radius, fill, stroke) {
@@ -77,7 +107,7 @@ var Renderer = function(viewport, interpreter, width) {
     this.width * interpreter.map.width, this.width * interpreter.map.height);
   
   // TODO hold sprite sheets somewhere else and refactor code
-  var loadCount = 2;
+  var loadCount = 3;
   var self = this;
   function handleLoad() {
     loadCount --;
@@ -93,6 +123,11 @@ var Renderer = function(viewport, interpreter, width) {
   arrowImage.src = 'img/arrow.svg';
   arrowImage.onload = handleLoad;
   this.arrowImage = arrowImage;
+  
+  var commandImage = new Image();
+  commandImage.src = 'img/command.svg';
+  commandImage.onload = handleLoad;
+  this.commandImage = commandImage;
 }
 
 Renderer.prototype.reset = function() {
@@ -108,7 +143,7 @@ Renderer.prototype.reset = function() {
   }, this);
   this.canvases.get('background').fillRect(0, 0, 
     this.canvases.width, this.canvases.height);
-  this.canvases.get('text').fillStyle = "#aaa";
+  this.canvases.get('text').fillStyle = "#555";
   // Redraw all tiles
   for(var y = 0; y < this.interpreter.map.height; ++y) {
     for(var x = 0; x < this.interpreter.map.width; ++x) {
@@ -154,20 +189,36 @@ Renderer.prototype.updateTile = function(x, y) {
         var segment = segmentMap[tile.directions[key].segment%4];
         var pathPos = pathMap[key];
         this.canvases.get('path').globalAlpha = tile.directions[key].unlikely ?
-          0.3 : 1;
+          0.2 : 1;
         this.canvases.get('path').drawImage(this.pathImage,
           (segment[0]*4+pathPos[0]) * 100, (segment[1]*3+pathPos[1]) * 100,
           100, 100, 0, 0, this.width, this.width);
       }
     }
     
-    if(cacheTile.direction != tile.direction) {
+    if(cacheTile.direction != tile.direction 
+      || cacheTile.command != tile.command) {
       cacheTile.direction = tile.direction;
-      this.canvases.get('arrow').clearRect(0, 0, this.width, this.width);
+      cacheTile.command = tile.command;
+      var arrowCtx = this.canvases.get('arrow');
+      arrowCtx.clearRect(0, 0, this.width, this.width);
       var arrowPos = arrowMap[tile.direction];
-      this.canvases.get('arrow').drawImage(this.arrowImage,
+      arrowCtx.drawImage(this.arrowImage,
         arrowPos[0] * 100, arrowPos[1] * 100,
         100, 100, 0, 0, this.width, this.width);
+      var commandPos = commandMap[tile.command];
+      arrowCtx.drawImage(this.commandImage,
+        commandPos[0] * 100, commandPos[1] * 100,
+        100, 100, 0, 0, this.width, this.width);
+      if(tile.data) {
+        var text = tile.data;
+        if(tile.command != 'push') text = Hangul.final[tile.data];
+        arrowCtx.font = (this.width*0.3)+"px sans-serif";
+        arrowCtx.textAlign = "right";
+        arrowCtx.textBaseline = "bottom";
+        arrowCtx.fillStyle = "#fff";
+        arrowCtx.fillText(text, this.width - 3, this.width - 3);
+      }
     }
     
     this.canvases.forEach(function(ctx) {
