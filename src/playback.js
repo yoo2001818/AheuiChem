84ff220d66d221613ea58c550c270e3b3730fdd1
Duknam Yoo <yoo2001818@gmail.com>
2015-07-22 15:30:15 +0900

    Fix playback buttons

--- src/playback.js
+++ src/playback.js
@@ -4,6 +4,7 @@ function Playback(interpreter, renderer, callback, resetCallback) {
   this.callback = callback;
   this.resetCallback = resetCallback;
   this.running = false;
+  this.playing = false;
   this.registerEvents();
 }
 
@@ -12,32 +13,73 @@ Playback.prototype.registerEvents = function() {
   // TODO let's not use getElementById in classes
   document.getElementById('icon-play').onclick = function() {
     document.activeElement.blur();
+    self.playing = true;
     self.running = true;
+    self.update();
   };
   document.getElementById('icon-pause').onclick = function() {
     document.activeElement.blur();
+    if(!self.playing) return;
     self.running = false;
+    self.update();
   };
   document.getElementById('icon-step').onclick = function() {
     document.activeElement.blur();
+    self.playing = true;
     self.step();
     self.running = false;
+    self.update();
   };
   document.getElementById('icon-stop').onclick = function() {
+    self.playing = false;
+    self.running = false;
     self.resetCallback();
+    self.update();
   };
   setInterval(function() {
     if (!self.running) return;
     self.step();
   }, 20);
+  self.update();
+}
+
+Playback.prototype.update = function() {
+  // TODO PLEASE CLEANUP THIS CODE
+  if(this.playing) {
+    // Stop button is enabled
+    document.getElementById('icon-stop').className = 'icon';
+    if(this.running) {
+      // Play button is selected
+      document.getElementById('icon-play').className = 'icon selected';
+      document.getElementById('icon-pause').className = 'icon';
+      document.getElementById('icon-step').className = 'icon disabled';
+    } else {
+      // Pause button is selected
+      document.getElementById('icon-play').className = 'icon';
+      document.getElementById('icon-pause').className = 'icon selected';
+      document.getElementById('icon-step').className = 'icon';
+    }
+  } else {
+    // Stop, pause button is disabled
+    document.getElementById('icon-stop').className = 'icon disabled';
+    document.getElementById('icon-play').className = 'icon';
+    document.getElementById('icon-pause').className = 'icon disabled';
+  }
 }
 
 Playback.prototype.step = function() {
   if(!this.interpreter || !this.renderer) return;
   this.interpreter.next();
   this.renderer.render();
-  if(this.interpreter.state.breakpoint) this.running = false;
-  if(!this.interpreter.state.running) this.running = false;
+  if(this.interpreter.state.breakpoint) {
+    this.running = false;
+    this.update();
+  }
+  if(!this.interpreter.state.running) {
+    this.running = false;
+    this.stopped = true;
+    this.update();
+  }
   if(this.callback) this.callback();
 }
 
